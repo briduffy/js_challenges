ed9790a75d9b256c06da4762869d1706f1525d15
Brianne Duffy <duffymbrianne@gmail.com>
2018-10-05 13:28:16 -0600

    string to array

--- challenges.rb
+++ challenges.rb
@@ -1,9 +1,20 @@
 #Alphabetize
 #Return a string with characters in alaphabetical order.
 #
-def alphabetize(str)
-  input = str.downcase.gsub(" ","").split("")
-  output = input.sort
-end
+#def alphabetize(str)
+#  input = str.downcase.gsub(" ","").split("")
+#  output = input.sort
+#end
+#
+#puts alphabetize("My name is Brianne.")
+#
+# Returns ".aabeeiimmnnnrsy"
 
-puts alphabetize("My name is Brianne.")
+#String to an Array
+#
+#def array(str)
+#  input = str.gsub("", " ")
+#end
+#
+#puts array("Brianne.")
+# Returns " B r i a n n e ."
